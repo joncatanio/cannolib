ec3035f1d528eb414708669dc0625741168b1432
Jon Catanio <joncatanio@gmail.com>
2018-02-03 18:53:50 -0800

    Removed useless comment

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,3 @@ mod value;
 pub use value::Value;
 mod numeric_type;
 pub use numeric_type::NumericType;
-
-// Logical NOT provided as a translation for Python's `not` keyword. Rust only
-// provides one op to be overloaded and cannolib has used that as Bitwise NOT.
